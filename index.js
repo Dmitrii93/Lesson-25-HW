a2c009823616396a84f30675eec59bfc35c4adb7
Dmitrii Bledniuc <ingoodshape93@gmail.com>
2020-06-03 19:46:24 +0300

    update

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     console.log(data);
     let title = document.querySelector('.title');
     title.innerText += ' ' +  data.title;
-    let src = document.querySelector('.image');
-    src.src = 'https://apod.nasa.gov/apod/image/2006/SarsCov2_Niaid_960.jpg';
-    src.alt += ' ' +  data.title;
-    let expl = document.querySelector('.explanation');
-    expl.innerText += ' ' +  data.explanation;
     let mt = document.querySelector('.media_type');
     mt.innerText += ' ' +  data.media_type;
+    if (data.media_type === 'video') {
+
+        let frame = document.createElement('iframe')
+        frame.src = data.url;
+        let figure = document.querySelector('figure');
+        figure.innerHTML = '';
+        figure.appendChild(frame);
+    }
+    else {
+
+        let src = document.querySelector('.image');
+        src.src = data.url;
+        src.alt += ' ' +  data.title;
+    }
+
+    let expl = document.querySelector('.explanation');
+    expl.innerText += ' ' +  data.explanation;
+
     let cr = document.querySelector('#copyright');
     if (data.copyright) {
         cr.innerText += ' ' + data.copyright;
